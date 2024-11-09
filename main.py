272c9f773749259bbca1ce11140847bec6e445c4
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-09 18:23:07 +0530

    running 1:05:00

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -11,5 +12,6 @@ def get_posts():
     return {"message": "This is your posts"}
 
 @app.post("/createpost")
-def create_posts():
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
     return {"message": "The post has been created Successfully"}
