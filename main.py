7204c261de7066e2af849b5802c7f29a40612dbd
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-13 12:26:56 +0530

    running 1:32:00

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class Post(BaseModel):
     published: bool =True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},{"title": "favorite food", "content": "I like pizza", "id": 2}]
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
