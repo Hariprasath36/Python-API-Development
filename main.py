b667630c46a69a68541109c0f6c626ff5d2ec92e
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-13 17:37:40 +0530

    running 2:11:00

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ def find_post(id):
         if p["id"] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -27,7 +32,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/createpost")
+@app.post("/createpost",status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
    post_dict = post.dict()
    post_dict["id"] = randrange(0, 1000000)
@@ -41,3 +46,14 @@ def get_post(id: int, response: Response):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND
         , detail = f"post with id : {id} was not found")
     return {"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND
+        , detail = f"post with id : {id} was not found")
+    
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
