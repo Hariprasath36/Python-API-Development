b173f5c17952b30af1f6715b5d49d0a7e441af18
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-14 12:33:58 +0530

    running 2:21:00

--- main.py
+++ main.py
@@ -62,5 +62,14 @@ def delete_post(id: int):
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
+    
+    index = find_index_post(id)
 
-    return {'message':"updated Post"}
+    if index == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND
+        , detail = f"post with id : {id} was not found")
+    post_dict = post.dict()
+    post_dict['id']=id
+    my_posts[index]= post_dict
+
+    return {"data": post_dict}
