dd5c6f847da809404c76bf51da7e1cd21cf8541a
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-23 11:59:41 +0530

    bug

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@1234localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:hari1172@localhost/fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.expression import null
-from sqlalchemy.sql.sqltypes import TIMESTAMP
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -9,5 +9,4 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, server_default='TRUE', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')) 
+    published = Column(Boolean,nullable=False)
