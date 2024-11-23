d8fcd64b083b6ab9b4149f442cb0a4f3a33074a5
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-23 16:09:04 +0530

    running 5:23:00

--- app/main.py
+++ app/main.py
@@ -66,6 +66,7 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
 #    new_post = cursor.fetchone()
 
 #    conn.commit()
+   new_post = models.Post(**post.dict())
    new_post =models.Post(title=post.title, content=post.content, published=post.published)
    db.add(new_post)
    db.commit()
@@ -73,26 +74,28 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
    return {"data": new_post}
 
 @app.get("/posts/{id}")
-def get_post(id: str):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
-    post=cursor.fetchone()
-    
+def get_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
+    # post=cursor.fetchone()
+        
+    post = db.query(models.Post).filter(models.Post.id==id).first()
     if not post:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND
         , detail = f"post with id : {id} was not found")
     return {"post_detail": post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s returning *""", (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-
-    if deleted_post == None:
+def delete_post(id: int ,db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s returning *""", (str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND
         , detail = f"post with id : {id} was not found")
     
-    
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
