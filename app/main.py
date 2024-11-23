dfb16cddcd66350fbded2a78d918c6b2987a00dc
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-23 16:59:49 +0530

    running 5:35:00

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
+
 from typing import Optional
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 from .database import engine,get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -16,10 +16,8 @@ app = FastAPI()
 
 
 
-class Post(BaseModel):
-    title: str
-    content: str  
-    published: bool =True
+
+    
 while True:
     try:
         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='password123', cursor_factory=RealDictCursor)
@@ -61,7 +59,7 @@ def get_posts(db: Session = Depends(get_db)):
     return {"data": my_posts}
 
 @app.post("/createpost",status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
 #    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,(post.title, post.content, post.published)) 
 #    new_post = cursor.fetchone()
 
@@ -99,7 +97,7 @@ def delete_post(id: int ,db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post,db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.Post,db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",(post.title,post.content,post.published, str(id)))
 
     # updated_post = cursor.fetchone()
