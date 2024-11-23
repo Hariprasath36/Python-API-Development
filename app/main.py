f36d9b2f14e6b3fcbbd3a1dc4a3ad6044fedf4fd
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-23 11:12:52 +0530

    running 4:50:00

--- app/main.py
+++ app/main.py
@@ -7,7 +7,19 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 
+from . import models
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 class Post(BaseModel):
     title: str
