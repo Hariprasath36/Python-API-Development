13f61f99396797958755daa42ca2fef2aaf1c870
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-27 17:06:48 +0530

    7:34:00 running

--- app/oauth2.py
+++ app/oauth2.py
@@ -13,12 +13,12 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def verify_token(token: str, credentials_exception):
+def verify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id: str = payload.get("user_id")
@@ -28,6 +28,8 @@ def verify_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
     
+    return token_data
+    
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
@@ -35,4 +37,4 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    return verify_token(token, credentials_exception)
+    return verify_access_token(token, credentials_exception)
