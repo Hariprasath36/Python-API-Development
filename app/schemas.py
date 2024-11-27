185446530369bb3a15926d3ef754a0b49c36e126
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-27 16:37:19 +0530

    7:23:00 running

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
-
+from typing import Optional
 
 class PostBase(BaseModel):
     title: str
@@ -38,3 +38,12 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str]
