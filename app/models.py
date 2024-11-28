ef94638695b067f33f7971a26d5886284b83375f
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-28 12:16:05 +0530

    8:08:00 running

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class Post(Base):
     published = Column(Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
+    
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False)
