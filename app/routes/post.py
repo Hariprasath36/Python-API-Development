127a8fa53c643c600bfe6c67d403dc20112d7c19
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-29 13:50:59 +0530

    10:27:00 running

--- app/routes/post.py
+++ app/routes/post.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from ..database import get_db
 from typing import List, Optional
+from sqlalchemy import func
 
 
 router =APIRouter(
@@ -12,12 +13,14 @@ router =APIRouter(
 )
 
 
-@router.get("/",response_model=List[schemas.Post])
+@router.get("/",response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user),limit:int=10, skip:int =0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
     # my_posts = cursor.fetchall()
     
-    posts=db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).limit(limit).offset(skip).all()
+    # posts=db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).limit(limit).offset(skip).all()
+    posts=db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+
     return posts
 
 @router.post("/",status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
