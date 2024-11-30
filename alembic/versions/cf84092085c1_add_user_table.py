d157da29412d34ff3ab5f39cc75222b979214466
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-11-30 15:04:14 +0530

    running

--- alembic/versions/cf84092085c1_add_user_table.py
+++ alembic/versions/cf84092085c1_add_user_table.py
@@ -19,9 +19,8 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade():
-    op.create_table('users', sa.Column('id', sa.Integer(), nullable=False, primary_key=True),
-                    sa.Column('email', sa.String(), nullable=False),
-                    sa.Column('password', sa.String(), nullable=False)),sa.Column('created_at', sa.TIMESTAMP(timezone=True),server_default=sa.text('now()'), nullable=False),sa.PrimaryKeyConstraint('id'),sa.UniqueConstraint('email')
+    op.create_table('users',sa.Column('id',sa.Integer(),nullable=False),sa.Column('email',sa.String(),nullable=False),sa.Column('password',sa.String(),nullable=False),sa.Column('created_at',sa.TIMESTAMP(timezone=True),server_default=sa.text('now()'),nullable=False),sa.PrimaryKeyConstraint('id'),sa.UniqueConstraint('email'),sa.UniqueConstraint('id'))
+    
     pass
 
 
