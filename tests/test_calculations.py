ed85e6b3541f07d697b74415041be66dd47f3d03
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-01 14:39:16 +0530

    14:36:00 running

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,16 @@
-from app.calculations import add
+from app.calculations import add, divide, multiply, subtract
 def test_add():
     print("testing add function")
-    sum = add(5,3)
-    assert sum == 8
+    assert add(5,3)== 8
+
+def test_subtract():
+    print("testing subtract function")
+    assert subtract(5,3)== 2
+
+def test_multiply():
+    print("testing multiply function")
+    assert multiply(5,3)== 15
+
+def test_divide():
+    print("testing divide function")
+    assert divide(20,5)== 4
