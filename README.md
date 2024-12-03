bea8aa946900e08a23b45e54a583ec1790d92f32
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-03 20:16:27 +0530

    15:18:00 running

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Python-API-Development
 # Run code :
  uvicorn app.main:app --reload
+
+ # Pytest :
+ pytest
+
+ # pytest for specific file :
+ pytest -v -s tests/test_user.py 
+
+ # Pytest disable warning :
+ pytest --disable-warnings
+
+ # Pytest stop if one test fails :
+ pytest --disable-warnings -v -x
--- tests/test_user.py
+++ tests/test_user.py
@@ -4,7 +4,7 @@ from app.main import app
 client = TestClient(app)
 
 def test_root():
-    res = client.get("/")
-    print(res)
-    # assert response.status_code == 200
-    # assert response.json() == {"message": "Hello World"}
+    response = client.get("/")
+   
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
