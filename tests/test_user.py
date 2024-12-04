ca3ec820f1455cf4febdf51a466243b601d058c4
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-04 08:37:12 +0530

    16:12:00 running

--- tests/test_user.py
+++ tests/test_user.py
@@ -20,4 +20,10 @@ def test_create_user(client):
    
 def test_login_user(test_user,client):
     res=client.post("/login",data={"username":test_user['email'],"password":test_user['password']})   
-   
+    login_res=schemas.Token(**res.json())
+    payload=jwt.decode(login_res.access_token,settings.secret_key,algorithms=(settings.algorithm))
+    id=payload.get("user_id")
+    assert id==test_user['id']
+    assert login_res.token_type=="bearer"
+    assert res.status_code==200
+ 
