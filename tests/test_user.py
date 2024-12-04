6bf43caab691612553e0b2d6cfe83aaf4d7cdadc
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-12-04 08:22:55 +0530

    16:00:00 running

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,42 +1,23 @@
-from fastapi.testclient import TestClient
-from app.main import app
+
+import pytest
+from jose import jwt
 from app import schemas
 from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.database import get_db
 
+    
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+# def test_root(client):
+#     res=client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message')=='Hello World'
+#     assert res.status_code==200
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
-
-
-client = TestClient(app)
-
-def test_root():
-    response = client.get("/")
-    print(response.json().get("message"))
-    assert response.json().get("message") == "Hello World"
-    assert response.status_code == 200
-
-def test_create_user():
-    response = client.post("/users/", json={"email": "hp@gmail.com", "password": "string123"})
-    new_user = schemas.UserOut(**response.json())
-    assert new_user.email == "hp@gmail.com"
-    assert response.status_code == 201
+def test_create_user(client):
+    res=client.post("/users/",json={"email":"hello123@example.com","password":"password123"})   
+    new_user = schemas.UserOut(**res.json()) 
+    assert new_user.email =='hello123@example.com'  
+    assert res.status_code==201    
+   
+def test_login_user(test_user,client):
+    res=client.post("/login",data={"username":test_user['email'],"password":test_user['password']})   
+   
